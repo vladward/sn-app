3429fd7125a3fc32e35c98a15a59705ecf18f296
vladward <vladward13@gmail.com>
2021-10-14 13:01:37 +0300

    feature: create Profile component

--- src/App.tsx
+++ src/App.tsx
@@ -2,34 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile.tsx/Profile";
 
 function App() {
     return (
         <div className="app-wrapper">
             <Header/>
             <Navbar/>
-          <div className='content'>
-            <div>
-              <img src="https://tinypng.com/images/social/website.jpg" alt=""/>
-            </div>
-          <div>
-            ava + description
-          </div>
-          <div>
-            My posts
-            <div>
-              New post
-            </div>
-            <div>
-              <div>
-                post 1
-              </div>
-              <div>
-                post 2
-              </div>
-            </div>
-          </div>
-        </div>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Profile.tsx/Profile.tsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://tinypng.com/images/social/website.jpg" alt=""/>
+            </div>
+            <div>
+                ava + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
