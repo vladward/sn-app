db533c24ffeb1a3675c813108fd68be385509bf4
vladward <vladward13@gmail.com>
2021-10-14 14:48:36 +0300

    feature: add Post component

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,22 +1,17 @@
 import React from "react"
 import s from "./MyPosts.module.css"
+import Post from "./Post/Post"
 
 const MyPosts = () => {
     return (
         <div>
             My posts
             <div>
-                New post
+                <textarea></textarea>
+                <button>Add post</button>
             </div>
             <div className={s.posts}>
-                <div className={s.item}>
-                    <img src="https://i.pinimg.com/originals/64/3f/47/643f47695ee94faf67e95c4587a61f55.jpg" alt=""/>
-                    post 1
-                </div>
-                <div className={s.item}>
-                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ12Slh8dRFuHXHnmUZfBs4KmMn5omrMwd8Kg&usqp=CAU" alt=""/>
-                    post 2
-                </div>
+                <Post/>
             </div>
         </div>
     )
---
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -0,0 +1,13 @@
+import s from "./Post.module.css";
+import React from "react";
+
+const Post = () => {
+    return (
+        <div className={s.item}>
+            <img src="https://i.pinimg.com/originals/64/3f/47/643f47695ee94faf67e95c4587a61f55.jpg" alt=""/>
+            post 1
+        </div>
+    )
+}
+
+export default Post
