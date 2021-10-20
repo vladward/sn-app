37cf75dc5cde1b04cf87c71fb0b0bc3deb5239e1
vladward <vladward13@gmail.com>
2021-10-20 16:57:28 +0300

    feature: add navbar links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,10 +5,10 @@ const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a>Profile</a>
+                <a href="/profile">Profile</a>
             </div>
             <div className={`${s.item} ${s.active}`}>
-                <a>Messages</a>
+                <a href="/dialogs">Messages</a>
             </div>
             <div className={s.item}>
                 <a>News</a>
