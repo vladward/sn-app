a62050f3b0179c863b5b3c097d331849eb1604e9
vladward <vladward13@gmail.com>
2021-10-20 17:06:25 +0300

    feature: add typescript react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
 
 function App() {
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <Navbar/>
-            <div className="app-wrapper-content">
-                <Dialogs/>
+        <BrowserRouter>
+            <div className="app-wrapper">
+                <Header/>
+                <Navbar/>
+                <div className="app-wrapper-content">
+                    <Route path='/dialogs' component={Dialogs}/>
+                    <Route path='/profile' component={Profile}/>
+                    <Dialogs/>
+                </div>
             </div>
-
-            {/*<Profile/>*/}
-        </div>
+        </BrowserRouter>
     );
 }
 
