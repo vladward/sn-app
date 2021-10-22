5dff98c16d6a7bb436d5f15f773d6da385d1b1e9
vladward <vladward13@gmail.com>
2021-10-22 14:39:39 +0300

    feature: add dialogs structure

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,10 +1,25 @@
 import React from "react"
+import { NavLink } from "react-router-dom"
 import s from "./Dialogs.module.css"
 
 const Dialogs = (props: any) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to="/dialogs/1">vlad</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/2">andrei</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/3">yuzik</NavLink>
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>Bye</div>
+            </div>
         </div>
     )
 }
