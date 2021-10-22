c2feac1a497e4d513b5a579227ac2534f7b6a70f
vladward <vladward13@gmail.com>
2021-10-22 16:01:52 +0300

    feature: add local state from MyPosts

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,6 +2,14 @@ import React from "react"
 import s from "./MyPosts.module.css"
 import Post from "./Post/Post"
 
+
+let postState = [
+    {id: 1, message: 'Hi how are you', likeCount: 3},
+    {id: 2, message: 'Yes no problem', likeCount: 6},
+
+]
+
+
 const MyPosts = () => {
     return (
         <div className={s.postsBlock}>
@@ -15,8 +23,8 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message={'Hi how are you'} likeCount={3}/>
-                <Post message={'Yes no problem'} likeCount={6}/>
+                <Post message={postState[0].message} likeCount={postState[0].likeCount}/>
+                <Post message={postState[1].message} likeCount={postState[0].likeCount}/>
             </div>
         </div>
     )
