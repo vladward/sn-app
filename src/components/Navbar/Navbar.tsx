93a9d644d5ee0ae1c71868e72d610d5d71aca312
vladward <vladward13@gmail.com>
2021-10-22 14:40:32 +0300

    fix: fix link to NavLink in Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,21 @@
 import React from "react"
 import s from "./Navbar.module.css"
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a href="/profile">Profile</a>
+                <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
             </div>
             <div className={`${s.item} ${s.active}`}>
-                <a href="/dialogs">Messages</a>
+                <NavLink to="/dialogs" activeClassName={s.active}>Messages</NavLink>
             </div>
             <div className={s.item}>
-                <a>News</a>
+                <NavLink to="#">News</NavLink>
             </div>
             <div className={s.item}>
-                <a>Settings</a>
+                <NavLink to="#">Settings</NavLink>
             </div>
         </nav>
     )
