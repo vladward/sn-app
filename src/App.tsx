3d072876a652349ed60f371572759d1e7155249d
vladward <vladward13@gmail.com>
2021-10-22 16:41:13 +0300

    fix: fix Route component code

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,8 @@ function App() {
                 <Header/>
                 <Navbar/>
                 <div className="app-wrapper-content">
-                    <Route path='/dialogs' component={Dialogs}/>
-                    <Route path='/profile' component={Profile}/>
+                    <Route path='/dialogs' render={() => <Dialogs />}/>
+                    <Route path='/profile' render={() => <Profile />}/>
                 </div>
             </div>
         </BrowserRouter>
