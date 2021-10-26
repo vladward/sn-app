bf40d36dfd51cc7f9f09adc4e8d6676aedf02e59
vladward <vladward13@gmail.com>
2021-10-26 15:06:36 +0300

    feature: add App types

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,29 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Profile from "./components/Profile/Profile";
-import Dialogs from "./components/Dialogs/Dialogs";
+import Profile, {ProfileType} from "./components/Profile/Profile";
+import Dialogs, {DialogType, MessageType} from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
 
-function App() {
+
+type AppPropsType = {
+    dialogs: Array<DialogType>
+    posts: Array<ProfileType>
+    messages: Array<MessageType>
+}
+
+const App = (props: AppPropsType) => {
     return (
         <BrowserRouter>
             <div className="app-wrapper">
                 <Header/>
                 <Navbar/>
                 <div className="app-wrapper-content">
-                    <Route path='/dialogs' render={() => <Dialogs />}/>
-                    <Route path='/profile' render={() => <Profile />}/>
+                    <Route path='/dialogs'
+                           render={ () => <Dialogs dialogs={props.dialogs}
+                                                   messages={props.messages}/> }/>
+                    <Route path='/profile'
+                           render={ () => <Profile posts={props.posts} /> }/>
                 </div>
             </div>
         </BrowserRouter>
