643ee22c49ec442feb3f9be1d84c18a0866882fd
vladward <vladward13@gmail.com>
2021-10-26 15:07:02 +0300

    feature: add DialogsItem types

--- src/components/Dialogs/DialogItem/DialogsItem.tsx
+++ src/components/Dialogs/DialogItem/DialogsItem.tsx
@@ -1,14 +1,15 @@
 import React from "react"
 import {NavLink} from "react-router-dom"
 import s from "./../Dialogs.module.css"
+import {DialogType} from "../Dialogs";
 
-const DialogItem = (props: any) => {
+const DialogItem = (props: DialogType) => {
 
     let path = "/dialogs/" + props.id
 
     return (
         <div className={s.dialog + ' ' + s.active}>
-            <img src={props.im} alt=""/>
+            <img src={props.src} alt=""/>
             <NavLink to={path}>{props.name}</NavLink>
         </div>
     )
