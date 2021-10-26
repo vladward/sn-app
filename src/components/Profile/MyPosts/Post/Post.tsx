d590b5e3670c13294d13efbd362feb80dc73d660
vladward <vladward13@gmail.com>
2021-10-26 15:10:04 +0300

    ref: refactor types from Post

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,12 +1,8 @@
 import s from "./Post.module.css";
 import React from "react";
+import {ProfileType} from "../../Profile";
 
-type PostMessageType = {
-    message: string
-    likeCount: number
-}
-
-const Post = (props: PostMessageType) => {
+const Post = (props: ProfileType) => {
     return (
         <div className={s.item}>
             <img src="https://i.pinimg.com/originals/64/3f/47/643f47695ee94faf67e95c4587a61f55.jpg" alt=""/>
