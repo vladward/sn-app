296bd8f3ff3614e655383436bd9b7e9ec25ea1c3
vladward <vladward13@gmail.com>
2021-10-26 15:09:03 +0300

    feature: spend props in app component

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import state from "./redux/state";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App dialogs={state.dialogsPage.dialogs}
+         messages={state.dialogsPage.messages}
+         posts={state.profilePage.posts}
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
