a5704a1ddeb75f350e74efe9d572d4b098b60f02
vladward <vladward13@gmail.com>
2021-10-26 17:31:25 +0300

    feat: add addPost button logic

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,26 +1,35 @@
-import React from "react"
+import React, {createRef} from "react"
 import s from "./MyPosts.module.css"
 import Post from "./Post/Post"
 import {ProfileType} from "../Profile";
 
 type MyPostsType = {
     posts: Array<ProfileType>
+    addPost: (postMessage: any) => void
 }
 
 const MyPosts = (props: MyPostsType) => {
+    let newPostElement = createRef<HTMLInputElement>()
+    let postsElements = props.posts.map( p => <Post message={p.message} likeCount={p.likeCount} id={p.id}/> )
+
+    const addPost = () => {
+        let text = newPostElement.current
+        props.addPost(text)
+    }
+
     return (
         <div className={s.postsBlock}>
             <h3>My Posts</h3>
             <div>
                 <div>
-                    <textarea></textarea>
+                    <input ref={newPostElement}></input>
                 </div>
                 <div>
-                    <button>Add post</button>
+                    <button onClick={addPost}>Add post</button>
                 </div>
             </div>
             <div className={s.posts}>
-                {props.posts.map( p => <Post message={p.message} likeCount={p.likeCount} id={p.id}/> )}
+                {postsElements}
             </div>
         </div>
     )
