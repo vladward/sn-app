ff5dbec8816b2b9ff0736d09fff595fea4ea843a
vladward <vladward13@gmail.com>
2021-10-30 22:03:07 +0300

    upd: update props for App

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
-import Profile, {ProfileType} from "./components/Profile/Profile";
+import Profile from "./components/Profile/Profile";
 import Dialogs, {DialogType, MessageType} from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
+import {ProfileType} from "./components/Profile/MyPosts/MyPosts";
 
 
 type AppPropsType = {
-    addPost: (postMessage: string) => void
-    dialogs: Array<DialogType>
     posts: Array<ProfileType>
+    newPostText: string
+    addPost: () => void
+    dialogs: Array<DialogType>
     messages: Array<MessageType>
+    updateNewPostText: (newText: string) => void
 }
 
 const App = (props: AppPropsType) => {
@@ -25,7 +28,10 @@ const App = (props: AppPropsType) => {
                            render={ () => <Dialogs dialogs={props.dialogs}
                                                    messages={props.messages}/> }/>
                     <Route path='/profile'
-                           render={ () => <Profile posts={props.posts} addPost={props.addPost}/> }/>
+                           render={ () => <Profile newPostText={props.newPostText}
+                                                   posts={props.posts}
+                                                   updateNewPostText={props.updateNewPostText}
+                                                   addPost={props.addPost}/> }/>
                 </div>
             </div>
         </BrowserRouter>
