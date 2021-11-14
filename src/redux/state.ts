447c6ae383e9e3b5f18361892b4e020a0e05ec83
vladward <vladward13@gmail.com>
2021-11-14 19:01:47 +0300

    ref: refactor state to store

--- src/redux/state.ts
+++ src/redux/state.ts
@@ -14,50 +14,50 @@ export type RootStateType = {
     dialogsPage: DialogPageType
 }
 
-let rerenderEntireTree = (state: RootStateType) => {
-    console.log('state change')
-}
 
-export let state: RootStateType = {
-    profilePage: {
-         posts: [
-            {id: 1, message: "Hi how are you", likeCount: 3},
-            {id: 2, message: "Yes no problem", likeCount: 6},
-            {id: 3, message: "Ok, let's go", likeCount: 8},
-        ],
-        newPostText: ''
+
+let store = {
+    _state: {
+        profilePage: {
+            posts: [
+                {id: 1, message: "Hi how are you", likeCount: 3},
+                {id: 2, message: "Yes no problem", likeCount: 6},
+                {id: 3, message: "Ok, let's go", likeCount: 8},
+            ],
+            newPostText: ''
+        },
+        dialogsPage: {
+            dialogs: [
+                {id: 1, name: "vlad", src: "https://avatars.githubusercontent.com/u/86200275?v=4"},
+                {id: 2, name: "andrei", src: "https://sun9-82.userapi.com/impf/c851420/v851420101/ee006/xGkm5WXSTwQ.jpg?size=1620x2160&quality=96&sign=7f437383d44301927bca846393723c80&type=album"},
+                {id: 3, name: "yuzik", src: "https://dnepr.news/images/cache/imagescacheimagesthumbNewstext/c-349-233-koryavchenkov-1.webp"}
+            ],
+            messages: [
+                {id: 1, message: "Hi"},
+                {id: 2, message: "Bye"}
+            ]
+        }
     },
-    dialogsPage: {
-        dialogs: [
-            {id: 1, name: "vlad", src: "https://avatars.githubusercontent.com/u/86200275?v=4"},
-            {id: 2, name: "andrei", src: "https://sun9-82.userapi.com/impf/c851420/v851420101/ee006/xGkm5WXSTwQ.jpg?size=1620x2160&quality=96&sign=7f437383d44301927bca846393723c80&type=album"},
-            {id: 3, name: "yuzik", src: "https://dnepr.news/images/cache/imagescacheimagesthumbNewstext/c-349-233-koryavchenkov-1.webp"}
-        ],
-        messages: [
-            {id: 1, message: "Hi"},
-            {id: 2, message: "Bye"}
-        ]
+    rerenderEntireTree(state: RootStateType) {
+        console.log('state change')
+    },
+    addPost() {
+        let newPost: ProfileType = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likeCount: 1
+        }
+        this._state.profilePage.posts.push(newPost)
+        this._state.profilePage.newPostText = ''
+        this.rerenderEntireTree(this._state)
+    },
+    updateNewPostText(newText: string) {
+        this._state.profilePage.newPostText = newText
+        this.rerenderEntireTree(this._state)
+    },
+    subscribe(observer: any) {
+        this.rerenderEntireTree = observer
     }
 }
 
-export let addPost = () => {
-    let newPost: ProfileType = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likeCount: 1
-    }
-    state.profilePage.posts.push(newPost)
-    state.profilePage.newPostText = ''
-    rerenderEntireTree(state)
-}
-
-export const updateNewPostText = (newText: string) => {
-    state.profilePage.newPostText = newText
-    rerenderEntireTree(state)
-}
-
-export const subscribe = (observer: any) => {
-    rerenderEntireTree = observer
-}
-
-export default state
+export default store
