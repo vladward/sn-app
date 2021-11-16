8d084e858df884cf6333449eaa843cd899107499
vladward <vladward13@gmail.com>
2021-11-16 15:34:47 +0300

    fix: flux render fix

--- src/render.tsx
+++ src/render.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {rerenderEntireTree} from "./render";
-import store from './redux/state'
+import store from "./redux/state";
 
-store.subscribe(rerenderEntireTree)
-rerenderEntireTree()
+export let rerenderEntireTree = () => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <App store={store} />
+        </React.StrictMode>,
+        document.getElementById('root')
+    )
+}
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
