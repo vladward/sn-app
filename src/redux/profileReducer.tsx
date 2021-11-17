ee45cb07077adb4cbc94d3da709d30cd3387407a
vladward <vladward13@gmail.com>
2021-11-17 19:41:26 +0300

    ref: profileReducer logic refactor

--- src/redux/profileReducer.tsx
+++ src/redux/profileReducer.tsx
@@ -1,7 +1,33 @@
 import React from "react"
+import {ActionType, AddPostActionType, ProfileType, RootStateType, UpdateNewPostTextActionType} from "./state";
 
-export const profileReducer = (state, action) => {
+const ADD_POST = 'ADD_POST'
+const UPDATE_NEW_POST_TEXT = 'UPDATE_NEW_POST_TEXT'
 
 
-    return state
+export const profileReducer = (state: RootStateType, action: ActionType) => {
+    switch (action.type) {
+        case ADD_POST:
+            let newPost: ProfileType = {
+                id: 5,
+                message: state.profilePage.newPostText,
+                likeCount: 1
+            }
+            state.profilePage.posts.push(newPost)
+            state.profilePage.newPostText = ''
+            return state
+
+        case UPDATE_NEW_POST_TEXT:
+            state.profilePage.newPostText = action.newText
+            return state
+
+        default:
+            return state
+    }
 }
+
+export const addPostActionCreator = (): AddPostActionType => ({type: ADD_POST})
+export const updateNewPostTextActionCreator = (newText: string): UpdateNewPostTextActionType => ({
+    type: UPDATE_NEW_POST_TEXT,
+    newText
+})
