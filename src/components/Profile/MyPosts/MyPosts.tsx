bd0bfe81079fbc7d4d6504c1bb62627a5546cf56
vladward <vladward13@gmail.com>
2021-11-17 15:06:00 +0300

    feat: import actions creators to MyPosts

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,35 +1,29 @@
 import React, {ChangeEvent} from "react"
 import s from "./MyPosts.module.css"
 import Post from "./Post/Post"
-import {ActionType} from "../../../redux/state";
+import {ActionType, addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/state";
 
 export type ProfileType = {
     id: number
     message: string
     likeCount: number
 }
-
 type MyPostsType = {
     posts: Array<ProfileType>
     newPostText: string
     dispatch: (action: ActionType) => void
 }
 
-
 const MyPosts = (props: MyPostsType) => {
-    // let newPostElement = React.createRef<HTMLTextAreaElement>()
     let postsElements = props.posts.map(p => <Post key={p.id} message={p.message} likeCount={p.likeCount} id={p.id}/>)
 
     const addPost = () => {
-        props.dispatch({type: 'ADD-POST'})
-        // props.addPost()
+        props.dispatch(addPostActionCreator())
     }
 
     const onPostChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-        //let text = newPostElement.current ? newPostElement.current.value : ''
-        //props.updateNewPostText(props.newPostText)
-        props.dispatch({type: 'UPDATE-NEW-POST-TEXT', newText: e.currentTarget.value})
-        // props.updateNewPostText(e.currentTarget.value)
+        let newText = e.currentTarget.value
+        props.dispatch(updateNewPostTextActionCreator(newText))
     }
 
     return (
