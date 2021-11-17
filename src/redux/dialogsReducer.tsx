47db42678525bc107cdc645d3dcab90ff4222d6d
vladward <vladward13@gmail.com>
2021-11-17 19:42:57 +0300

    ref: dialogsReducer logic refactor

--- src/redux/dialogsReducer.tsx
+++ src/redux/dialogsReducer.tsx
@@ -1,7 +1,29 @@
 import React from "react"
+import {MessageType, SendMessageActionType, UpdateNewMessageBodyActionType} from "./state";
 
-export const dialogsReducer = (state, action) => {
+const SEND_MESSAGE = 'SEND_MESSAGE'
+const UPDATE_NEW_MESSAGE_BODY = 'UPDATE_NEW_MESSAGE_BODY'
 
+export const dialogsReducer = (state:any, action:any) => {
 
-    return state
+switch (action.type) {
+    case SEND_MESSAGE:
+        let mess: MessageType = {
+            id: 6,
+            message: state.newMessageBody
+        }
+        state.messages.push(mess)
+        state.newMessageBody = ''
+        return state
+
+    case UPDATE_NEW_MESSAGE_BODY:
+        state.newMessageBody = action.body
+        return state
+    default:
+        return state
+    }
 }
+
+
+export const sendMessageActionCreator = (): SendMessageActionType => ({type: SEND_MESSAGE})
+export const updateNewMessageBodyActionCreator = (body: string): UpdateNewMessageBodyActionType => ({type: UPDATE_NEW_MESSAGE_BODY, body})
