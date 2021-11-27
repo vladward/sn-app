445706d87e1bc8660294518c9f179be2e4596501
vladward <vladward13@gmail.com>
2021-11-27 21:42:22 +0300

    feat: add myPostsContainer

---
+++ src/components/Profile/MyPosts/MyPostsContainer.tsx
@@ -0,0 +1,47 @@
+import React, {ChangeEvent} from "react"
+import s from "./MyPosts.module.css"
+import Post from "./Post/Post"
+import {ActionType} from "../../../redux/store";
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/profileReducer";
+
+export type ProfileType = {
+    id: number
+    message: string
+    likeCount: number
+}
+type MyPostsType = {
+    posts: Array<ProfileType>
+    newPostText: string
+    dispatch: (action: ActionType) => void
+}
+
+const MyPosts = (props: MyPostsType) => {
+    let postsElements = props.posts.map(p => <Post key={p.id} message={p.message} likeCount={p.likeCount} id={p.id}/>)
+
+    const addPost = () => {
+        props.dispatch(addPostActionCreator())
+    }
+
+    const onPostChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        let newText = e.currentTarget.value
+        props.dispatch(updateNewPostTextActionCreator(newText))
+    }
+
+    return (
+        <div className={s.postsBlock}>
+            <h3>My Posts</h3>
+            <div>
+                <div>
+                    <textarea placeholder={'Text'} onChange={onPostChange} value={props.newPostText}/>
+                </div>
+                <div>
+                    <button onClick={addPost}>Add post</button>
+                </div>
+            </div>
+            <div className={s.posts}>
+                {postsElements}
+            </div>
+        </div>
+    )
+}
+export default MyPosts
