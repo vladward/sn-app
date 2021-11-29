4fe543a64849bbe6f843c9a8fdce3171b3720dad
vladward <vladward13@gmail.com>
2021-11-29 18:55:07 +0300

    feat: Posts types refactor

--- src/components/Profile/MyPosts/Post/Post.tsx
+++ src/components/Profile/MyPosts/Post/Post.tsx
@@ -1,8 +1,12 @@
 import s from "./Post.module.css";
 import React from "react";
-import {ProfileType} from "../MyPosts";
+import {PostsType} from "../../../../redux/profileReducer";
 
-const Post = (props: ProfileType) => {
+
+const Post = (props: PostsType) => {
+
+
+
     return (
         <div className={s.item}>
             <img src="https://i.pinimg.com/originals/64/3f/47/643f47695ee94faf67e95c4587a61f55.jpg" alt=""/>
