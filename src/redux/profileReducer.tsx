deddf27ea29027eb3b0af4409b921c2a7cc0c860
vladward <vladward13@gmail.com>
2021-11-29 19:06:51 +0300

    feat: take out state types for profileReducer from unused state component

--- src/redux/profileReducer.tsx
+++ src/redux/profileReducer.tsx
@@ -1,6 +1,35 @@
 const ADD_POST = 'ADD_POST'
 const UPDATE_NEW_POST_TEXT = 'UPDATE_NEW_POST_TEXT'
 
+export type AddPostActionType = {
+    type: 'ADD_POST'
+}
+export type UpdateNewPostTextActionType = {
+    type: 'UPDATE_NEW_POST_TEXT'
+    newText: string
+}
+
+export type ProfileActionType = AddPostActionType | UpdateNewPostTextActionType
+
+export type ProfilePageType = {
+    posts: PostsType[]
+    newPostText: string
+}
+export type PostsType = {
+    id: number
+    message: string
+    likeCount: number
+}
+export type ProfileInitialStateType = typeof profileInitialState
+
+let profileInitialState: ProfilePageType = {
+    posts: [
+        {id: 1, message: "Hi how are you", likeCount: 3},
+        {id: 2, message: "Yes no problem", likeCount: 6},
+        {id: 3, message: "Ok, let's go", likeCount: 8},
+    ],
+    newPostText: ''
+}
 
 export const profileReducer = (state: ProfilePageType, action: ActionType) => {
     switch (action.type) {
