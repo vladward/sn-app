9b8f277e3fad4fd8b3d256116df86584fd37ffc8
vladward <vladward13@gmail.com>
2021-11-29 19:03:22 +0300

    ref: take out dialogsPage state from unused store in dialogsReducer

--- src/redux/dialogsReducer.tsx
+++ src/redux/dialogsReducer.tsx
@@ -25,15 +25,37 @@ export type DialogsPageType = {
 }
 export type DialogsInitialStateType = typeof dialogsInitialState
 
-switch (action.type) {
-    case SEND_MESSAGE:
-        let mess: MessageType = {
-            id: 6,
-            message: state.newMessageBody
-        }
-        state.messages.push(mess)
-        state.newMessageBody = ''
-        return state
+let dialogsInitialState: DialogsPageType = {
+        dialogs: [
+            {id: 1, name: "vlad", src: "https://avatars.githubusercontent.com/u/86200275?v=4"},
+            {
+                id: 2,
+                name: "andrei",
+                src: "https://sun9-82.userapi.com/impf/c851420/v851420101/ee006/xGkm5WXSTwQ.jpg?size=1620x2160&quality=96&sign=7f437383d44301927bca846393723c80&type=album"
+            },
+            {
+                id: 3,
+                name: "yuzik",
+                src: "https://dnepr.news/images/cache/imagescacheimagesthumbNewstext/c-349-233-koryavchenkov-1.webp"
+            }
+        ],
+        messages: [
+            {id: 1, message: "Hi"},
+            {id: 2, message: "Bye"}
+        ],
+        newMessageBody: ''
+}
+
+export const dialogsReducer = (state: DialogsInitialStateType = dialogsInitialState, action: DialogsActionType): DialogsInitialStateType => {
+    switch (action.type) {
+        case SEND_MESSAGE:
+            let mess: MessagesType = {
+                id: 6,
+                message: state.newMessageBody
+            }
+            state.messages.push(mess)
+            state.newMessageBody = ''
+            return state
 
     case UPDATE_NEW_MESSAGE_BODY:
         state.newMessageBody = action.body
