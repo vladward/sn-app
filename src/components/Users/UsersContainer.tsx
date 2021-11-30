ce87625cadccfd391f97232e0c43d7e5bd9937ef
vladward <vladward13@gmail.com>
2021-11-30 18:04:32 +0300

    feat: user container refactor

---
+++ src/components/Users/UsersContainer.tsx
@@ -0,0 +1,41 @@
+import {Users} from "./Users";
+import {connect} from "react-redux";
+import {Dispatch} from "redux";
+import {AppStateType} from "../../redux/redux-store";
+import {followAC, setUsersAC, unFollowAC, UsersInitialStateType, UsersType} from "../../redux/usersReducer";
+
+type UsersMapStateToPropsType = {
+    users: UsersType[]
+}
+type UsersMapDispatchToPropsType = {
+    follow: (id: string) => void
+    unFollow: (id: string) => void
+    setUsers: (users: UsersType[]) => void
+}
+
+export type UsersContainerType = UsersMapStateToPropsType & UsersMapDispatchToPropsType
+
+
+const mapStateToProps = (state: AppStateType): UsersMapStateToPropsType => {
+    return {
+        users: state.usersPage.users
+    }
+}
+
+const mapDispatchToProps = (dispatch: Dispatch) => {
+    return {
+        follow: (id: string) => {
+            dispatch(followAC(id))
+        },
+        unFollow: (id: string) => {
+            dispatch(unFollowAC(id))
+        },
+        setUsers: (users: UsersType[]) => {
+            dispatch(setUsersAC(users))
+        }
+    }
+}
+
+const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
+
+export default UsersContainer
