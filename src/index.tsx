f376905889f7e7ba882d9f9137f334f7a0bf33a0
vladward <vladward13@gmail.com>
2021-11-30 15:49:27 +0300

    fix: remove rerenderEntireTree from project

--- src/index.tsx
+++ src/index.tsx
@@ -6,22 +6,14 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import {Provider} from "react-redux";
 
-
-export let rerenderEntireTree = () => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </React.StrictMode>,
-        document.getElementById('root')
-    )
-}
-rerenderEntireTree()
-store.subscribe(() => {
-    rerenderEntireTree()
-})
-
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
