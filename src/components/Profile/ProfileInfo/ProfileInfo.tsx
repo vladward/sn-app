14970518da23e9c756a34aafe8b5e25117dcda28
vladward <vladward13@gmail.com>
2021-12-02 15:32:15 +0300

    fix: rename profile image classname

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -6,7 +6,7 @@ const ProfileInfo = (props:any) => {
     return (
         <div>
             <div>
-                <img src="https://tinypng.com/images/social/website.jpg" alt=""/>
+                <img className={s.profileImage} src="https://tinypng.com/images/social/website.jpg" alt=""/>
             </div>
             <div className={s.descriptionBlock}>
                 ava + description
