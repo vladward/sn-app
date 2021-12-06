fa7285972e078c8721c2dd0986c79ab2693e5875
vladward <vladward13@gmail.com>
2021-12-06 21:37:20 +0300

    feat: use initial values in get response

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -11,7 +11,7 @@ export class Users extends React.Component<UsersContainerType> {
     }
 
     componentDidMount() {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users")
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
             .then(response => {
                 this.props.setUsers(response.data.items)
                 console.log(response)
