be88146959d465e1b8972619c9f3a55a620551f6
vladward <vladward13@gmail.com>
2021-12-06 22:23:18 +0300

    feat: add initial request on Users componentDidMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -14,6 +14,7 @@ export class Users extends React.Component<UsersContainerType> {
         axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
             .then(response => {
                 this.props.setUsers(response.data.items)
+                this.props.setTotalUsersCount(response.data.totalCount)
                 console.log(response)
             })
     }
