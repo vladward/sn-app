74f34d0ae764aa7661450b196623278fa618ac1f
vladward <vladward13@gmail.com>
2021-12-06 22:15:01 +0300

    feat: add setCurrentPageHandler and take him request for take a currentPage from server

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -24,42 +24,54 @@ export class Users extends React.Component<UsersContainerType> {
     UnFollowHandler = (id: string) => {
         this.props.unFollow(id)
     }
+    setCurrentPageHandler = (page: number) => {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${page}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+                console.log(response)
+            })
+        this.props.setCurrentPage(page)
+    }
 
     render() {
 
         let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize)
         let pages = []
-        for(let i = 1; i <= pagesCount; i++) {
+        for (let i = 1; i <= pagesCount; i++) {
             pages.push(i)
         }
 
         return (
             <div>
                 <div>{pages.map((p, i) => {
-                    return <span key={i} className={this.props.currentPage === p ? s.currentPage : ''}>{p}</span>
-                })}</div>
+                        return <span key={i}
+                                     onClick={() => this.setCurrentPageHandler(p)}
+                                     className={this.props.currentPage === p ? s.currentPage : ''}
+                        >{p}</span>
+                    }
+                )}</div>
                 {this.props.users.map(u => <div key={u.id}>
-                <span>
+                    <span>
                     <div className={s.userPhotoContainer}>
-                        <img src={u.photos[0] ? u.photos[0] : noPhoto} alt="photo" className={s.userPhoto}/>
+                    <img src={u.photos[0] ? u.photos[0] : noPhoto} alt="photo" className={s.userPhoto}/>
                     </div>
                     <div>
-                        {u.followed
-                            ? <button onClick={() => this.UnFollowHandler(u.id)}>unfollow</button>
-                            : <button onClick={() => this.followHandler(u.id)}>follow</button>
-                        }
+                {u.followed
+                    ? <button onClick={() => this.UnFollowHandler(u.id)}>unfollow</button>
+                    : <button onClick={() => this.followHandler(u.id)}>follow</button>
+                }
                     </div>
-                </span>
+                    </span>
                     <span>
                     <span>
-                        <div>{u.name}</div>
-                        <div>{u.status}</div>
+                    <div>{u.name}</div>
+                    <div>{u.status}</div>
                     </span>
                         {/*<span>*/}
                         {/*    <div>{u.location.city}</div>*/}
                         {/*    <div>{u.location.country}</div>*/}
                         {/*</span>*/}
-                </span>
+                    </span>
                 </div>)}
             </div>
         )
