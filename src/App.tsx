c059cdf3a189fbaf768fb1071802ceea3e7fe1f5
vladward <vladward13@gmail.com>
2021-12-07 21:28:19 +0300

    fix: Profile route path fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar/Navbar";
 import {BrowserRouter, Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import {ProfileContainer} from "./components/Profile/ProfileContainer";
+import ProfileContainer from "./components/Profile/ProfileContainer";
 
 const App = () => {
     return (
@@ -16,7 +16,7 @@ const App = () => {
                 <div className="app-wrapper-content">
                     <Route path='/dialogs'
                            render={ () => <DialogsContainer /> }/>
-                    <Route exact path='/profile'
+                    <Route path='/profile/:userId?'
                            render={ () => <ProfileContainer /> }/>
                     <Route path='/users'
                            render={ () => <UsersContainer /> }/>
