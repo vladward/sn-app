6a79fd5819c00d7a68fd75d686b749328025060f
vladward <vladward13@gmail.com>
2021-12-07 21:30:01 +0300

    ref: add types

--- src/components/Profile/ProfileContainer.tsx
+++ src/components/Profile/ProfileContainer.tsx
@@ -2,13 +2,38 @@ import React from "react"
 import Profile from "./Profile";
 
 type ProfileMapStateToPropsType = {
-
+    profile: ProfileType
 }
 type ProfileMapDispatchToPropsType = {
-
+    setUserProfile: (profile: ProfileType) => void
+}
+export type PathParamsType = {
+    userId: string
+}
+type ContactsType = {
+    facebook: string | null,
+    website: string | null,
+    vk: string | null,
+    twitter: string | null,
+    instagram: string | null,
+    youtube: string | null,
+    github: string | null,
+    mainLink: string | null
 }
 
-type ProfileContainerType = ProfileMapStateToPropsType & ProfileMapDispatchToPropsType
+type PhotosType = {
+    small: string | null,
+    large: string | null
+}
+export type ProfileType = {
+    aboutMe: string,
+    contacts: ContactsType,
+    lookingForAJob: boolean,
+    lookingForAJobDescription: string | null,
+    fullName: string,
+    userId: number,
+    photos: PhotosType
+}
 
 export class ProfileContainer extends React.Component<ProfileContainerType>{
 
