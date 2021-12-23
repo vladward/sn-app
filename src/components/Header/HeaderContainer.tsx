16e5ab7879d5fb57917eaab6644e26d646aa49a3
vladward <vladward13@gmail.com>
2021-12-23 18:37:51 +0300

    feat: add request for user data

--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -1,10 +1,25 @@
 import React from "react"
 import {Header} from "./Header";
+import axios from "axios";
+import {connect} from "react-redux";
+import {setUserDataAC} from "../../redux/authReducer";
 
 class HeaderContainer extends React.Component<any, any> {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
+            withCredentials: true
+        })
+            .then(response => {
+                if (response.data.resultCode === 0) {
+                    let {id, email, login} = response.data.data
+                    this.props.setUserDataAC(id, email, login)
+                }
+            })
+    }
+
     render() {
         return (
-            <Header />
+            <Header/>
         )
     }
 }
