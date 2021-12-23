771966ddc6de3db8b74408471a079a1dd3c9d08a
vladward <vladward13@gmail.com>
2021-12-23 18:37:11 +0300

    feat: add state to HeaderContainer with connect

--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -9,4 +9,11 @@ class HeaderContainer extends React.Component<any, any> {
     }
 }
 
-export default HeaderContainer
+const mapStateToProps = (state: any) => {
+    return {
+        isAuth: state.auth.isAuth,
+        login: state.auth.login
+    }
+}
+
+export default connect(mapStateToProps, {setUserDataAC})(HeaderContainer)
