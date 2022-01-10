b37693ff43c4d1bc6a5bbe172f0e560effa5f5ed
vladward <vladward13@gmail.com>
2022-01-10 15:36:14 +0300

    feat: add Login page

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
+import {Login} from "./components/Login/Login";
 
 const App = () => {
     return (
@@ -20,6 +21,8 @@ const App = () => {
                            render={ () => <ProfileContainer /> }/>
                     <Route path='/users'
                            render={ () => <UsersContainer /> }/>
+                    <Route path='/login'
+                           render={ () => <Login /> }/>
                 </div>
             </div>
         </BrowserRouter>
