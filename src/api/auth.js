37e909eca2f545efc246c0827e6732b02843bba0
vladward <vladward13@gmail.com>
2022-01-16 21:38:47 +0300

    feat: create login\logout api methods

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,3 +3,9 @@ import {instance} from "./instance";
 export const authMe = () => {
     return instance.get(`auth/me`)
 }
+export const login = (email, password, rememberMe = false) => {
+    return instance.post(`auth/login`, {email, password, rememberMe})
+}
+export const logout = () => {
+    return instance.delete('auth/login')
+}
