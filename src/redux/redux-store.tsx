2b2395fb328b1f9aeca30ef4893b9950748aaebd
vladward <vladward13@gmail.com>
2022-01-18 23:51:42 +0300

    feat: combine app reducer in store

--- src/redux/redux-store.tsx
+++ src/redux/redux-store.tsx
@@ -5,17 +5,19 @@ import {UsersGeneralActionType, usersReducer} from "./usersReducer";
 import {AuthGeneralActionType, authReducer} from "./authReducer";
 import thunkMiddleware, {ThunkAction} from 'redux-thunk'
 import {reducer as formReducer} from 'redux-form'
+import {AppGeneralActionType, appReducer} from "./appReducer";
 
 const rootReducer = combineReducers({
     profilePage: profileReducer,
     dialogsPage: dialogsReducer,
     usersPage: usersReducer,
     auth: authReducer,
+    app: appReducer,
     form: formReducer,
 })
 
 export type AppStateType = ReturnType<typeof rootReducer>
 
-export type AppActionsType = AuthGeneralActionType | DialogsActionType | ProfileActionType | UsersGeneralActionType
+export type AppActionsType = AuthGeneralActionType | DialogsActionType | ProfileActionType | UsersGeneralActionType | AppGeneralActionType
 export type ThunkType<ReturnType = void> = ThunkAction<ReturnType, AppStateType, unknown, AppActionsType>
 export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
